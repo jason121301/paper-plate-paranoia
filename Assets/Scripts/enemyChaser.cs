3a44f1e4ae7a4c9a50ce3a9cc642699d303c85a7
swannercjj <j.chen1904@gmail.com>
2024-10-26 16:09:01 -0400

    enemy chaser movement

--- Assets/Scripts/enemyChaser.cs
+++ Assets/Scripts/enemyChaser.cs
@@ -6,21 +6,57 @@ using UnityEngine.UIElements;
 public class enemyMovement : MonoBehaviour
 {
     private GameObject player;
-    public float speed = 3.0f;
+    [SerializeField] float speed = 5.0f;
 
-    private float distance;
+    [SerializeField] float waitingTime = 1.0f;
+    [SerializeField] float moveTime = 5.0f;
+    private float timer = 0f;
+    private bool isWaiting;
+
+    private Vector2 playerPos;
+
     // Start is called before the first frame update
     void Start()
     {
+        timer = waitingTime;
+        isWaiting = true;
         player = GameObject.Find("Player");
+        playerPos = player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        distance = Vector2.Distance(transform.position, player.transform.position);
-        Vector2 direction = player.transform.position - transform.position;
-
-        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            if (isWaiting)
+            {
+                // reached waiting time
+                timer = moveTime;   // reset
+                playerPos = player.transform.position; // new direction to go
+                isWaiting = false;  // can move
+            }
+            else
+            {
+                timer = waitingTime;
+                isWaiting = true;
+            }
+        }
+        if (!isWaiting)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, playerPos, speed * Time.deltaTime);
+        }
     }
+    
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.name == "Player")
+        {
+            Debug.Log("Game over");
+
+        }
+    }
+
 }
