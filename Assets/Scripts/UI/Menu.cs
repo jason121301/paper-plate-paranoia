101504aa9aeda11f80123fc5ea18ec033d5a44e5
tycelm <70135829+tycelm@users.noreply.github.com>
2024-10-27 14:53:47 -0400

    added menu scene

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject cutscene;
+    public GameObject menu;
+    public GameObject MenuImage;
+    public GameObject skipPrompt;
+    public int speed;
+    private bool move = false;
+    private bool readyToSkip = false;
+    public void PlayGame()
+    {
+
+        // move menu to the right
+        move = true;
+
+        // activate child with cutscene and scene loader
+        cutscene.SetActive(true);
+
+        Invoke("StartCutscene", 1);
+        Invoke("ShowSkip", 8);
+    }
+
+    private void ShowSkip()
+    {
+        skipPrompt.SetActive(true);
+
+        readyToSkip = true;
+    }
+
+    private void StartCutscene()
+    {
+        // turn off menu image so it doesnt overlap
+        MenuImage.SetActive(false);
+    }
+    private void Update()
+    {
+        if (move)
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ShowSkip();
+            if (readyToSkip)
+            {
+                // switch the game scene
+                SceneManager.LoadScene(1, LoadSceneMode.Single);
+            }
+        }
+    }
+
+    public void QuitGame()
+    {
+        // bye bye
+        Application.Quit();
+    }
+}
