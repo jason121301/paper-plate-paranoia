a7681a4717394550387b9044da60dc815e53adb7
unknown <j.chen1904@gmail.com>
2024-10-27 13:49:56 -0400

    ketchup bottle animation and stop moving

--- Assets/Scripts/enemy/KetchupEnemy.cs
+++ Assets/Scripts/enemy/KetchupEnemy.cs
@@ -14,20 +14,47 @@ public class KetchupEnemy : MonoBehaviour
     private float rand_x;
     private float rand_y;
 
+    private Animator anim;
+    private bool flash;
+
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         camera = Camera.main;
-        rand_x = Random.Range(0, 1);
-        rand_y = Random.Range(0, 1);
+        rand_x = Random.Range(0, 2);
+        rand_y = Random.Range(0, 2);
         //player = GameObject.Find("Player");
+
+        waitTime = anim.runtimeAnimatorController.animationClips[0].length;
+        flash = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 location = camera.ViewportToWorldPoint(new Vector3(rand_x, rand_y, camera.nearClipPlane));
+        elapsedTime += Time.deltaTime;
 
-        transform.position = location;
+        if (!flash)
+        {
+            Vector2 location = camera.ViewportToWorldPoint(new Vector3(rand_x, rand_y, camera.nearClipPlane));
+            transform.position = location + new Vector2(-location.x, -location.y) * 0.15f;
+
+            Vector2 direction = (transform.position - camera.transform.position).normalized;
+            float angle = Mathf.Atan2(direction.x, -direction.y) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        }
+
+
+        if (elapsedTime > waitTime)
+        {
+            Destroy(gameObject);
+        }
     }
+
+    public void OnAnimationFlash()
+    {
+        flash = true;
+    }
+
 }
