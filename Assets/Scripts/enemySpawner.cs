d59029deda227051518f73758a0598ff430d64bd
jason121301 <jason121301@gmail.com>
2024-10-26 22:00:46 -0400

    wall plus bg plus coffe mug

--- Assets/Scripts/enemySpawner.cs
+++ Assets/Scripts/enemySpawner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -41,44 +42,57 @@ public class enemySpawner : MonoBehaviour
         while (true)
         {
             yield return wait;
-            float rand_x = Random.Range(-rangeX,rangeX);
-            float rand_y = Random.Range(-rangeY,rangeY);
-            Vector2 location = new Vector2(rand_x, rand_y);
-            if (enemy == EnemyType.Chaser)
-            {
-                float rand_variable = Random.Range(0f, 1f);
-                float rand = Random.Range(0f, 1f);
-                if (rand_variable < 0.25)
-                {
-                    location = mainCamera.ViewportToWorldPoint(new Vector3(0f, rand, mainCamera.nearClipPlane));
-                }
-                else if (0.25 <= rand_variable && rand_variable < 0.5)
-                {
-                    location = mainCamera.ViewportToWorldPoint(new Vector3(1f, rand, mainCamera.nearClipPlane));
-                }
-                else if (0.5 <= rand_variable && rand_variable < 0.75)
-                {
-                    location = mainCamera.ViewportToWorldPoint(new Vector3(rand, 0f, mainCamera.nearClipPlane));
-                }
-                else if (0.75 <= rand_variable)
-                {
-                    location = mainCamera.ViewportToWorldPoint(new Vector3(rand, 1f, mainCamera.nearClipPlane));
-                }
-                
-            }
 
-            if (enemy == EnemyType.Shadow)
+            Vector2 location = getLocation();
+            while (Mathf.Abs(location.x) > 25 || Mathf.Abs(location.y) > 25)
             {
-                Vector3 middle = mainCamera.ScreenToWorldPoint(new Vector3(mainCamera.pixelWidth / 2, mainCamera.pixelHeight / 2, mainCamera.nearClipPlane));
-                location = new Vector2(middle.x + rand_x, middle.y + rand_y);
-            }
+                location = getLocation();
+                yield return null;
+            }       
             GameObject newEnemy = Instantiate(enemyToSpawn, location, Quaternion.identity);
             newEnemy.transform.parent = gameObject.transform;
-            initialRate = initialRate / rateIncrease;
+            initialRate = initialRate / rateIncrease;   
             
         }
     }
+
+    private Vector2 getLocation()
+    {
+        float rand_x = Random.Range(-rangeX, rangeX);
+        float rand_y = Random.Range(-rangeY, rangeY);
+        Vector2 location = new Vector2(rand_x, rand_y);
+        if (enemy == EnemyType.Chaser)
+        {
+            float rand_variable = Random.Range(0f, 1f);
+            float rand = Random.Range(0f, 1f);
+            if (rand_variable < 0.25)
+            {
+                location = mainCamera.ViewportToWorldPoint(new Vector3(0f, rand, mainCamera.nearClipPlane));
+            }
+            else if (0.25 <= rand_variable && rand_variable < 0.5)
+            {
+                location = mainCamera.ViewportToWorldPoint(new Vector3(1f, rand, mainCamera.nearClipPlane));
+            }
+            else if (0.5 <= rand_variable && rand_variable < 0.75)
+            {
+                location = mainCamera.ViewportToWorldPoint(new Vector3(rand, 0f, mainCamera.nearClipPlane));
+            }
+            else if (0.75 <= rand_variable)
+            {
+                location = mainCamera.ViewportToWorldPoint(new Vector3(rand, 1f, mainCamera.nearClipPlane));
+            }
+
+        }
+
+        if (enemy == EnemyType.Shadow)
+        {
+            Vector3 middle = mainCamera.ScreenToWorldPoint(new Vector3(mainCamera.pixelWidth / 2, mainCamera.pixelHeight / 2, mainCamera.nearClipPlane));
+            location = new Vector2(middle.x + rand_x, middle.y + rand_y);
+        }
+        return location;
+    }
 }
+
 
 public enum EnemyType
 {
